3c2edc4041f8c932ba65f21b5cdc45b80e87bb4c
CircuitCoder <circuitcoder0@gmail.com>
2014-01-05 17:55:04 +0800

    Add HomeServlet (In fact just testing my CI)

---
+++ src/main/java/tk/circuitcoder/grafixplane/servlet/HomeServlet.java
@@ -0,0 +1,15 @@
+package tk.circuitcoder.grafixplane.servlet;
+
+import javax.servlet.http.HttpServlet;
+
+import tk.circuitcoder.grafixplane.GrafixPlane;
+
+/**
+ * Generate one's HOME! How IMPORTANT!
+ * @author CircuitCoder
+ *
+ */
+public class HomeServlet extends HttpServlet {
+	private static final long serialVersionUID = GrafixPlane.VERSION;
+	
+}
